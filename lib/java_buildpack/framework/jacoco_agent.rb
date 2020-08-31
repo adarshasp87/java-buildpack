6211fce2d5773cd87cacfc0815ead8b8bd6077ec
CHANDAN K P <46045385+chandan-prakash@users.noreply.github.com>
2020-08-31 22:04:04 +0530

    Update jacoco_agent.rb

--- lib/java_buildpack/framework/jacoco_agent.rb
+++ lib/java_buildpack/framework/jacoco_agent.rb
@@ -28,7 +28,7 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
         download_zip false
-        @droplet.copy_resources(@droplet.additional_libraries)
+        @droplet.copy_resources
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
